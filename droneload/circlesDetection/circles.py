6f3d95afca9663450e59a42f3b3ee6b1496b8bc6
GregoireDourdin <gregoire.dourdin@student-cs.fr>
2023-10-10 17:33:06 +0200

    test

--- droneload/circlesDetection/circles.py
+++ droneload/circlesDetection/circles.py
@@ -1,32 +1,91 @@
 import cv2
 import numpy as np
+import math
 
 
-def circles_in_img(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (9, 9), 2)
+def circle_similarity(circle1, circle2, tol):
+    """
+    Calculate the similarity score between two circles based on IoU.
 
-    circles = cv2.HoughCircles(
-        gray,
-        cv2.HOUGH_GRADIENT,
-        dp=1,
-        minDist=20,
-        param1=50,
-        param2=30,
-        minRadius=0,
-        maxRadius=0,
-    )
+    Args:
+        circle1 (tuple): A tuple containing (x, y, radius) for the first circle.
+        circle2 (tuple): A tuple containing (x, y, radius) for the second circle.
+        tolerance (float): The similarity tolerance threshold (between 0 and 1).
 
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        circle_list = []
-        circle_info = []
+    Returns:
+        bool: True if the circles are similar, False otherwise.
+    """
+    x1, y1, r1 = circle1
+    x2, y2, r2 = circle2
 
-        for (x, y, r) in circles:
-            circle_list.append([x, y, r])
+    # Calculate the Intersection over Union (IoU) score
+    distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    iou = min(r1, r2) / max(r1, r2)
 
-        for i, circle in enumerate(circle_list):
-            circle_info.append(
-                (f"Circle {i + 1}: Center = ({circle[0]}, {circle[1]}), Radius = {circle[2]}"))
+    return iou >= tol
 
-    return circle_list
+
+def find_circles(img, tol):
+    """
+    Find circles in an image based on a specified similarity tolerance.
+
+    Args:
+        image_path (str): The path to the image.
+        tolerance (float): The similarity tolerance threshold (between 0 and 1).
+        min_radius (int): Minimum radius of circles to search for (default is 0).
+        max_radius (int): Maximum radius of circles to search for (default is None).
+
+    Returns:
+        list: A list of circles that meet the similarity threshold.
+    """
+    img_height, img_width = img.shape
+    img = cv2.GaussianBlur(img, (9, 9), 2)
+
+    min_radius = 0
+    max_radius = min(img_height, img_width)
+
+    # Initialize an accumulator matrix to store circle votes
+    accumulator = np.zeros((img_height, img_width, max_radius), dtype=np.uint8)
+
+    # Detect edges using Canny edge detection
+    edges = cv2.Canny(img, threshold1=50, threshold2=150)
+
+    # Find non-zero edge pixels (potential circle centers)
+    edge_pixels = np.argwhere(edges > 0)
+
+    # Iterate through potential circle centers
+    for center_y, center_x in edge_pixels:
+        for radius in range(min_radius, max_radius):
+            # Calculate circle parameters
+            theta = np.linspace(0, 2 * np.pi, 100)
+            x = center_x + radius * np.cos(theta)
+            y = center_y + radius * np.sin(theta)
+
+            # Ensure the circle coordinates are within the image bounds
+            x = np.clip(x, 0, img_width - 1).astype(int)
+            y = np.clip(y, 0, img_height - 1).astype(int)
+
+            # Vote for the circle in the accumulator
+            accumulator[y, x, radius] += 1
+
+    # Find circles that meet the similarity threshold
+    detected_circles = []
+    for radius in range(min_radius, max_radius):
+        for y in range(img_height):
+            for x in range(img_width):
+                if accumulator[y, x, radius] > 0:
+                    circle = (x, y, radius)
+                    # Check similarity with other detected circles
+                    is_similar = True
+                    for detected_circle in detected_circles:
+                        if circle_similarity(circle, detected_circle, tol):
+                            is_similar = False
+                            break
+                    if is_similar:
+                        detected_circles.append(circle)
+
+    return detected_circles
+
+
+image = cv2.imread('cirlces.jpeg')
+find_circles(image, 0.8)
