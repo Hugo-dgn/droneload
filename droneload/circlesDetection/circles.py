e8f96a479ecd4651ee0156905ec57138ceb7d1df
GregoireDourdin <gregoire.dourdin@student-cs.fr>
2023-10-24 16:43:47 +0200

    suite des modifications pour les tailles reelles de cercles, ca marche enfin, probleme d'affichage des rayons et distance (*10 sur l'image au lieu de 1 seul par cercle)

--- droneload/circlesDetection/circles.py
+++ droneload/circlesDetection/circles.py
@@ -8,9 +8,13 @@ real_radius = 2.8  # entrer ici taille reelle du cercle
 
 
 def load_calibration_data():
+    # Charger les données de calibration à partir du fichier calibration.yml
     with open(calibration_file, 'r') as file:
         calib_data = yaml.load(file, Loader=yaml.FullLoader)
 
+    # Accéder aux données de la matrice de caméra (mtx) et de la distorsion (dist)
+    # Accéder au premier élément du tuple (le dictionnaire)
+    calib_data = calib_data[0]
     camera_matrix = np.array(calib_data['mtx'])
     distortion_coeff = np.array(calib_data['dist'])
 
@@ -18,6 +22,7 @@ def load_calibration_data():
 
 
 def calculate_real_radius_and_distance(imgpts, camera_matrix, distortion_coeff):
+    imgpts = np.array(imgpts, dtype=float)  # Convertir en tableau NumPy
     angles = np.linspace(0, 2 * np.pi, 10)
     objpts = np.array([[real_radius * np.cos(theta), real_radius *
                       np.sin(theta), 0] for theta in angles], dtype=np.float32)
@@ -30,51 +35,53 @@ def calculate_real_radius_and_distance(imgpts, camera_matrix, distortion_coeff):
         return real_radius, distance
 
 
-def detect_circles_and_measure():
-    cap = cv2.VideoCapture(0)
+def detect_circles_and_measure(img):
+    circle_list = []
     camera_matrix, distortion_coeff = load_calibration_data()
 
-    while True:
-        ret, frame = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (9, 9), 2)
+    circles = cv2.HoughCircles(
+        gray,
+        cv2.HOUGH_GRADIENT,
+        dp=1,
+        minDist=20,
+        param1=10,
+        param2=100,
+        minRadius=10,
+        maxRadius=200
+    )
 
-        if not ret:
-            print("Erreur lors de la capture de l'image.")
-            break
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        imgpts = []  # Créez une liste vide pour stocker les coordonnées des cercles
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (9, 9), 2)
-        circles = cv2.HoughCircles(
-            gray,
-            cv2.HOUGH_GRADIENT,
-            dp=1,
-            minDist=20,
-            param1=10,
-            param2=100,
-            minRadius=10,
-            maxRadius=200
-        )
+        for (x, y, r) in circles:
+            circle_list.append([x, y, r])
 
-        if circles is not None:
-            circles = np.round(circles[0, :]).astype("int")
+            # Échantillonnez 10 points le long du contour du cercle
+            circle_points = []
+            for i in range(10):
+                angle = i * 2 * np.pi / 10
+                circle_x = int(x + r * np.cos(angle))
+                circle_y = int(y + r * np.sin(angle))
+                circle_points.append([circle_x, circle_y])
 
-            for (x, y, r) in circles:
-                imgpts = np.array([[x, y]], dtype=float)
+                imgpt = np.array([[circle_x, circle_y]], dtype=float)
+                imgpts.append(imgpt)
 
-                real_radius, distance = calculate_real_radius_and_distance(
-                    imgpts, camera_matrix, distortion_coeff)
+        # Reste du code pour calculer le rayon réel et la distance
+        real_radii, distances = calculate_real_radius_and_distance(
+            imgpts, camera_matrix, distortion_coeff)
+        print(real_radii, distances)
 
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frame, f"Rayon réel = {real_radius:.2f} cm", (
-                    x - r, y - r - 10), font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
-                cv2.putText(frame, f"Distance à la caméra = {distance:.2f} cm", (
-                    x - r, y - r + 20), font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        for i, (circle_x, circle_y) in enumerate(circle_points):
+            cv2.putText(img, f"R={real_radii:.2f} cm", (circle_x - r, circle_y - r - 10),
+                        font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(img, f"Distance={distances:.2f} cm", (circle_x - r, circle_y - r + 20),
+                        font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
 
-                cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+            cv2.circle(img, (x, y), r, (0, 255, 0), 4)
 
-        cv2.imshow("Circles Detection", frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    return circle_list
