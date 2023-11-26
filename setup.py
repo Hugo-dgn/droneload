e87c5006c2c1930e243ada5e3a96a0b20b502211
Hugo Degeneve <hugo.degeneve@gmail.com>
2023-11-26 18:06:53 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.12'
+VERSION = '0.1.13'
 DESCRIPTION = 'Provide tools to complete the tasks of the drone load competition.'
 
 with open("README.md", "r", encoding="utf-8") as f:
