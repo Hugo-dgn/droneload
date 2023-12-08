23d6c51ed3a1e2020f67d21ebc92ab15681c2c33
Hugo Degeneve <hugo.degeneve@gmail.com>
2023-12-08 12:26:14 +0100

    new version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.22'
+VERSION = '0.2.0'
 DESCRIPTION = 'Provide tools to complete the tasks of the drone load competition.'
 
 with open("README.md", "r", encoding="utf-8") as f:
